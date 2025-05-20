b4c5dda372ed4f5f166dd4ed90cc76aa01a02750
rang <withghd@gmail.com>
2025-05-20 16:46:32 +0900

    chore: RedisConfig 수정

--- src/main/java/team/project/fiverockrun/common/config/RedisConfig.java
+++ src/main/java/team/project/fiverockrun/common/config/RedisConfig.java
@@ -5,24 +5,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+    // 1. 문자열만 저장할 때 사용하는 템플릿 (락 등 단순 키-값용)
+    @Bean(name = "stringRedisTemplate")
+    public RedisTemplate<String, String> stringRedisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
-        // Redis 연결을 위한 팩토리 설정
         template.setConnectionFactory(connectionFactory);
-
-        // 키,값,해시 키, 해시 값 직렬화 방식을 문자열로 설정
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
-
         return template;
     }
 
+    // 2. 객체를 저장할 때 사용하는 템플릿 (Reservation 등)
+    @Bean(name = "objectRedisTemplate")
+    public RedisTemplate<String, Object> objectRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
 }
