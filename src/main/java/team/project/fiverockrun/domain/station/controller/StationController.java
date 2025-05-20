44641bd058b79c1d9f0a82f1afb62f98f3803c1f
dawn0920 <saoul0920@gmail.com>
2025-05-20 10:08:23 +0900

    feat: StationController 분리

--- src/main/java/team/project/fiverockrun/domain/station/controller/StationController.java
+++ src/main/java/team/project/fiverockrun/domain/station/controller/StationController.java
@@ -4,26 +4,23 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.project.fiverockrun.domain.station.dto.request.StationRequest;
 import team.project.fiverockrun.domain.station.dto.response.StationResponse;
 import team.project.fiverockrun.domain.station.service.StationService;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/stations")
 @RequiredArgsConstructor
 public class StationController {
 
     private final StationService stationService;
 
-    // 역 생성
-    @PostMapping("/admin/stations")
-    public ResponseEntity<StationResponse> createStation (
-            @Valid @RequestBody StationRequest stationRequest
-            ) {
-        StationResponse response = stationService.createStation(stationRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    // 역 조회
+    @GetMapping
+    public ResponseEntity<List<StationResponse>> getAllStation() {
+        return ResponseEntity.ok(stationService.getAllStation());
     }
-
 }
