afbe9c79a933de04e113491d534a66a96d9f936d
exmrim <exmrim@naver.com>
2025-05-20 15:38:30 +0900

    feat:RedisConfig 생성

---
+++ src/main/java/team/project/fiverockrun/common/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package team.project.fiverockrun.common.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        // Redis 연결을 위한 팩토리 설정
+        template.setConnectionFactory(connectionFactory);
+
+        // 키,값,해시 키, 해시 값 직렬화 방식을 문자열로 설정
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
+
+}
