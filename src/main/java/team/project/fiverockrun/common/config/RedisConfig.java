fd9e82ed2f4964b225a4968573c833ce4133ae8d
exmrim <exmrim@naver.com>
2025-05-21 18:39:31 +0900

    chore: RedisConfig 수정

--- src/main/java/team/project/fiverockrun/common/config/RedisConfig.java
+++ src/main/java/team/project/fiverockrun/common/config/RedisConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -13,7 +16,7 @@ public class RedisConfig {
 
     // 1. 문자열만 저장할 때 사용하는 템플릿 (락 등 단순 키-값용)
     @Bean
-    public RedisTemplate<String, String> stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
