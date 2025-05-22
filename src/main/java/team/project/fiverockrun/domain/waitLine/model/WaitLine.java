c2a7f270e3cbddc30a6d9f53e9698af0740dfc6b
exmrim <exmrim@naver.com>
2025-05-23 02:53:55 +0900

    feat: WaitLine 수정

--- src/main/java/team/project/fiverockrun/domain/waitLine/model/WaitLine.java
+++ src/main/java/team/project/fiverockrun/domain/waitLine/model/WaitLine.java
@@ -3,22 +3,19 @@ package team.project.fiverockrun.domain.waitLine.model;
 import lombok.*;
 
 import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class WaitLine implements Serializable {
     private Long userId;
-    private LocalDate departureDate;
-    private LocalTime departureTime;
-    private LocalDateTime arrivalDateTime;
+    private String departureDate;
+    private String departureTime;
+    private String arrivalDateTime;
     private Long departureStation;
     private Long arrivalStation;
     private int passengerCount;
 
-    public WaitLine(Long userId, LocalDate departureDate, LocalTime departureTime, LocalDateTime arrivalDateTime, Long departureStation, Long arrivalStation, int passengerCount) {
+    public WaitLine(Long userId, String departureDate, String departureTime, String arrivalDateTime, Long departureStation, Long arrivalStation, int passengerCount) {
         this.userId = userId;
         this.departureDate = departureDate;
         this.departureTime = departureTime;
