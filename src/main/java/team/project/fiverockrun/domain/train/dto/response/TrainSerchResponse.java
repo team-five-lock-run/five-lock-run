d829a6b5166bca673bacb5c95ba640f159f9ad04
dawn0920 <saoul0920@gmail.com>
2025-05-22 10:13:54 +0900

    feat: 열차 검색 dto

--- src/main/java/team/project/fiverockrun/domain/train/dto/response/TrainSerchResponse.java
+++ src/main/java/team/project/fiverockrun/domain/train/dto/response/TrainSerchResponse.java
@@ -2,16 +2,20 @@ package team.project.fiverockrun.domain.train.dto.response;
 
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Getter
 public class TrainSerchResponse {
 
+    private LocalDate date;
     private String trainType;          // 열차 종류
     private String trainNumber;        // 열차 번호
-    private String departureStation;   // 출발역
-    private String arrivalStation;     // 도착역
-    private LocalDateTime departureTime; // 출발시간
+    private Long departureStation;   // 출발역
+    private Long arrivalStation;     // 도착역
+    private LocalDate departureDate;    // 출발날짜
+    private LocalTime departureTime;    // 출발시간
     private LocalDateTime arrivalTime;   // 도착시간
     private int regularPrice;          // 일반실 가격
     private int premiumPrice;          // 특실 가격
