10544885553a56a7fe19d23af1d82b7379a79d8e
exmrim <exmrim@naver.com>
2025-05-22 16:06:01 +0900

    feat: WaitLine 수정

--- src/main/java/team/project/fiverockrun/domain/waitLine/model/WaitLine.java
+++ src/main/java/team/project/fiverockrun/domain/waitLine/model/WaitLine.java
@@ -1,26 +1,30 @@
 package team.project.fiverockrun.domain.waitLine.model;
 
 import lombok.*;
-import team.project.fiverockrun.domain.waitLine.dto.request.WaitLinesRequestDto;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Getter
 @RequiredArgsConstructor
 public class WaitLine implements Serializable {
     private Long userId;
-    private Long date;
-    private Long time;
-    private Long departureId;
-    private Long arrivalId;
-    private Integer people;
+    private LocalDate departureDate;
+    private LocalTime departureTime;
+    private LocalDateTime arrivalDateTime;
+    private Long departureStation;
+    private Long arrivalStation;
+    private int passengerCount;
 
-    public WaitLine(Long userId, Long date, Long time, Long departureId, Long arrivalId, Integer people) {
+    public WaitLine(Long userId, LocalDate departureDate, LocalTime departureTime, LocalDateTime arrivalDateTime, Long departureStation, Long arrivalStation, int passengerCount) {
         this.userId = userId;
-        this.date = date;
-        this.time = time;
-        this.departureId = departureId;
-        this.arrivalId = arrivalId;
-        this.people = people;
+        this.departureDate = departureDate;
+        this.departureTime = departureTime;
+        this.arrivalDateTime = arrivalDateTime;
+        this.departureStation = departureStation;
+        this.arrivalStation = arrivalStation;
+        this.passengerCount = passengerCount;
     }
 }
