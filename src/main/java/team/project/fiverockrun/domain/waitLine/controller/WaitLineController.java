43632cfef666113cda495ea73206ce3d12f94d8d
exmrim <exmrim@naver.com>
2025-05-22 16:07:06 +0900

    feat: WaitLineController 수정

--- src/main/java/team/project/fiverockrun/domain/waitLine/controller/WaitLineController.java
+++ src/main/java/team/project/fiverockrun/domain/waitLine/controller/WaitLineController.java
@@ -3,9 +3,12 @@ package team.project.fiverockrun.domain.waitLine.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import team.project.fiverockrun.domain.waitLine.dto.request.WaitLinesRequestDto;
+import team.project.fiverockrun.domain.auth.security.CustomUserPrincipal;
+import team.project.fiverockrun.domain.waitLine.dto.request.WaitLineRequestDto;
+import team.project.fiverockrun.domain.waitLine.dto.response.WaitLineResponseDto;
 import team.project.fiverockrun.domain.waitLine.service.WaitLineService;
 import team.project.fiverockrun.domain.waitLine.service.SseService;
 
@@ -18,23 +21,40 @@ public class WaitLineController {
     private final SseService sseService;
 
     @PostMapping("/enter")
-    public ResponseEntity<String> enterQueue(@RequestBody WaitLinesRequestDto requestDto) {
-        return ResponseEntity.ok(waitLineService.enterQueue(requestDto));
+    public ResponseEntity<WaitLineResponseDto> enterQueue(
+            @AuthenticationPrincipal CustomUserPrincipal customUserPrincipal,
+            @RequestBody WaitLineRequestDto requestDto
+    ) {
+        return ResponseEntity.ok(waitLineService.enterQueue(customUserPrincipal.getUser().getId(), requestDto));
     }
 
     @GetMapping("/position")
-    public ResponseEntity<Long> getPosition(@RequestParam Long userId, @RequestParam String key) {
-        return ResponseEntity.ok(waitLineService.getPosition(userId, key));
+    public ResponseEntity<Long> getPosition(
+            @AuthenticationPrincipal CustomUserPrincipal customUserPrincipal,
+            @RequestParam String key
+    ) {
+        return ResponseEntity.ok(waitLineService.getPosition(customUserPrincipal.getUser().getId(), key));
     }
 
     @PostMapping("/leave")
-    public ResponseEntity<String> leaveQueue(@RequestParam Long userId, @RequestParam String key) {
-        return ResponseEntity.ok(waitLineService.leaveQueue(userId, key));
+    public ResponseEntity<WaitLineResponseDto> leaveQueue(
+            @AuthenticationPrincipal CustomUserPrincipal customUserPrincipal,
+            @RequestParam String key
+    ) {
+        return ResponseEntity.ok(waitLineService.leaveQueue(customUserPrincipal.getUser().getId(), key));
     }
 
     @GetMapping(value="/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(Long userId) {
-        return sseService.subscribe(userId);
+    public SseEmitter subscribe(@AuthenticationPrincipal CustomUserPrincipal customUserPrincipal) {
+        return sseService.subscribe(customUserPrincipal.getUser().getId());
+    }
+
+    @PostMapping("/complete")
+    public ResponseEntity<WaitLineResponseDto> complete(
+            @AuthenticationPrincipal CustomUserPrincipal customUserPrincipal,
+            @RequestParam String key
+    ) {
+        return ResponseEntity.ok(waitLineService.completeQueue(customUserPrincipal.getUser().getId(), key));
     }
 
 
